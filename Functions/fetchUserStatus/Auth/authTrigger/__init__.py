dd518cbb6bb373fea962568911a03ba5bbc2482d
roharon <roharon98@gmail.com>
2021-05-22 23:49:46 +0900

    Modify: file structure

--- Functions/fetchUserStatus/Auth/authTrigger/__init__.py
+++ Functions/fetchUserStatus/Auth/authTrigger/__init__.py
@@ -1,11 +1,11 @@
 import logging
 
 import azure.functions as func
+import requests
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
     name = req.params.get('name')
     if not name:
         try:
@@ -22,3 +22,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+    tenant = "common"
+
+    requests.get("https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize"
+    .format(tenant=tenant))
+
+"""
+https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize?
+client_id=6731de76-14a6-49ae-97bc-6eba6914391e
+&response_type=code
+&redirect_uri=http%3A%2F%2Flocalhost%2Fmyapp%2F
+&response_mode=query
+&scope=offline_access%20user.read%20mail.read
+&state=12345
+"""
